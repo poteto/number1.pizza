0242ba6da0df3f7dfa689894581ddd21c75a9207
Lauren Tan <laurent@netflix.com>
2018-03-13 19:51:07 +1100

    Something rendering but no lighting

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,93 @@
 /// <reference path='./index.d.ts'/>
 import {
-  AmbientLight,
+  Color,
+  DirectionalLight,
   PerspectiveCamera,
-  PointLight,
   Scene,
   WebGLRenderer
 } from 'three';
+
 import MTLLoader from 'three/examples/js/loaders/MTLLoader';
 import OBJLoader from 'three/examples/js/loaders/OBJLoader';
+import OrbitControls from 'three/examples/js/controls/OrbitControls';
+import Detector from 'three/examples/js/Detector';
+
 import pizzaMtl from './models/pizza/pepperoni pizza.mtl';
 import pizzaObj from './models/pizza/pepperoni pizza.obj';
-import './models/pizza/pizzaTxt.png';
 
-let container, stats;
-let camera, scene, renderer;
-let mouseX = 0,
-  mouseY = 0;
-let windowHalfX, windowHalfY;
+import './models/pizza/pizzaTxt.png';
+import './style.css';
+
+let controls, camera, scene, renderer;
+
+if (!Detector.webgl) {
+  Detector.addGetWebGLMessage();
+}
 
 init();
 animate();
 
 function init() {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-  camera = new PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    1,
-    2000
-  );
-  camera.position.z = 250;
-
-  // scene
   scene = new Scene();
-  let ambientLight = new AmbientLight(0xcccccc, 0.4);
-  scene.add(ambientLight);
+  camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-  let pointLight = new PointLight(0xffffff, 0.8);
-  camera.add(pointLight);
-  scene.add(camera);
+  // setup scene and camera
+  scene.background = new Color().setHSL(0.6, 0, 1);
+  camera.position.z = 5;
+
+  // renderer
+  renderer = new WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // lighting
+  let keyLight = new DirectionalLight(new Color('hsl(30, 100%, 75%)'), 1.0);
+  keyLight.position.set(-100, 0, 100);
+
+  let fillLight = new DirectionalLight(new Color('hsl(240, 100%, 75%)'), 0.75);
+  fillLight.position.set(100, 0, 100);
+
+  let backLight = new DirectionalLight(0xffffff, 1.0);
+  backLight.position.set(100, 0, -100).normalize();
+
+  scene.add(keyLight);
+  scene.add(fillLight);
+  scene.add(backLight);
+
+  // orbit controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', render);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.25;
+  controls.enableZoom = true;
 
   // model
   let mtlLoader = new MTLLoader();
+  let objLoader = new OBJLoader();
   mtlLoader.setTexturePath('/');
   mtlLoader.setPath('/');
   mtlLoader.load(pizzaMtl, materials => {
     materials.preload();
-    let objLoader = new OBJLoader();
     objLoader.setMaterials(materials);
     objLoader.setPath('/');
     objLoader.load(pizzaObj, object => {
       scene.add(object);
-      object.position.y -= 60;
     });
   });
-
-  renderer = new WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  container.appendChild(renderer.domElement);
-
-  document.addEventListener('mousemove', onDocumentMouseMove, false);
-  window.addEventListener('resize', onWindowResize, false);
 }
 
 function animate() {
   requestAnimationFrame(animate);
+  controls.update();
   render();
 }
 
 function render() {
-  camera.position.x += (mouseX - camera.position.x) * 0.05;
-  camera.position.y += (-mouseY - camera.position.y) * 0.05;
-  camera.lookAt(scene.position);
   renderer.render(scene, camera);
-}
-
-function onWindowResize() {
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function onDocumentMouseMove(event) {
-  mouseX = (event.clientX - windowHalfX) / 2;
-  mouseY = (event.clientY - windowHalfY) / 2;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,19 @@ const path = require('path');
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.png$/,
@@ -21,6 +28,7 @@ module.exports = {
         test: /\.(obj|mtl)$/,
         use: 'file-loader'
       },
+      // https://github.com/mrdoob/three.js/issues/9562#issuecomment-372169712
       {
         test: require.resolve('three/examples/js/loaders/OBJLoader'),
         use: ['imports-loader?THREE=three', 'exports-loader?THREE.OBJLoader']
@@ -28,6 +36,20 @@ module.exports = {
       {
         test: require.resolve('three/examples/js/loaders/MTLLoader'),
         use: ['imports-loader?THREE=three', 'exports-loader?THREE.MTLLoader']
+      },
+      {
+        test: require.resolve('three/examples/js/controls/OrbitControls'),
+        use: [
+          'imports-loader?THREE=three',
+          'exports-loader?THREE.OrbitControls'
+        ]
+      },
+      {
+        test: require.resolve('three/examples/js/controls/Detector'),
+        use: [
+          'imports-loader?THREE=three',
+          'exports-loader?THREE.Detector'
+        ]
       }
     ]
   },
